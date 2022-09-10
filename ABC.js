6fc2baa470964612bd662a810ab70f0a35cd9ad4
Abhay-N-J <abhayjoshi029@gmail.com>
2022-09-10 16:45:33 +0530

    ABC update

--- ABC.js
+++ ABC.js
@@ -4,7 +4,7 @@ function init()
     interns = document.querySelectorAll("div")
     objs.addEventListener('click', (e) => {
             interns[0].style.display = "block";
-            const name = e.target.
+            // const name = e.target.
             for(j=1;j<interns.length;j++)
             {
                 console.log(interns[j].innerHTML+ name)
